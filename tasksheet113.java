4bff6db628fd04b1648e7b89dea7ea23cad97563
kioski21 <152053430+kioski21@users.noreply.github.com>
2024-12-11 06:05:51 +0000

    doneAddingOutput

--- tasksheet113.java
+++ tasksheet113.java
@@ -1,1 +1,27 @@
+public class tasksheet113 {
 
+    
+
+    public static void main(String[] args) {
+        
+                
+        int x = 1 , y = 2 ;  // result is now 3
+        int z = x + y ;
+        System.out.println(z);
+
+        z -=  1; // result is now 2
+        System.out.println(z);
+
+        z *=  2; // result is now 4
+        System.out.println(z);
+
+        z /=  2; // result is now 2
+        System.out.println(z);
+
+        z += 8; // result is now 10
+        System.out.println(z);
+        z %= 7; // result is now 3
+        System.out.println(z);
+
+    }
+}
