bbbd66ae8304d9accd678519446a7acaa1cd3e00
kioski21 <152053430+kioski21@users.noreply.github.com>
2024-12-11 05:26:30 +0000

    Done Adding output

--- tasksheet112.java
+++ tasksheet112.java
@@ -1,1 +1,22 @@
 
+public class tasksheet112 {
+
+    public static void main(String[] args) {
+     
+        byte  Three =  3; 
+        short  el = 1;
+        int o =  0; 
+        char w = 'W';
+        int o1 = 0 ; 
+        char r = 'R';
+        byte l2 = 1;
+        char d = 'D' ;
+        
+        float twoPointZero= 2.0f;
+        boolean t= true;
+        
+        String output = "H"+Three+el+el+o+"  "+w+o1+r+l2+d+" "+twoPointZero+"  "+t;
+
+        System.out.println(output);
+    }
+}
