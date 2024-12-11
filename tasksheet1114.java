87792c10316997bf1d46193fa94f125547d0e2c1
kioski21 <152053430+kioski21@users.noreply.github.com>
2024-12-11 13:12:33 +0000

    Done Creating output

---
+++ tasksheet1114.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class tasksheet1114 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    
+        
+        int checkValue ;
+        String message ; 
+        System.out.println("Odd or even identifier");
+        System.out.println("Enter a number ");
+        checkValue= scan.nextInt();
+
+        syntax: message=(checkValue%2==0 )?checkValue+" "+"is even number":checkValue+" "+"is odd number";
+        System.out.println(message);
+
+
+
+    }
+
+}
